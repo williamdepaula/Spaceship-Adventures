90b60a38da1d805c0437f2cc818e44f8dd0cd790
unknown <william.paula@dominio.ufms.br>
2018-11-29 15:30:06 -0300

    Criação do player.py para mantes a classe do jogador.

--- game.py
+++ game.py
@@ -2,101 +2,36 @@
 
 import pygame, sys, glob
 from pygame import *
+from player import Player
 
 
-
-WINDOW_HEIGHT, WINDOW_WIDTH = 800, 600
-screen = pygame.display.set_mode((WINDOW_HEIGHT, WINDOW_WIDTH), FULLSCREEN)
-pygame.init()
-pygame.mouse.set_visible(False)
-sound = pygame.mixer.Sound('music/musica.wav')
-
-
-class Player:
+class Game:
+    '''
+    Classe que cuida de todas as informações para rodar o jogo
+    '''
     def __init__(self):
-        sound.play()
-        self.movimento = {
-            'up': False,
-            'down': False,
-            'left': False,
-            'right': False,
-        }
-
-        self.ani = {
-            'up': glob.glob("recursos/player/nave-up-*.png"),
-            'left': glob.glob("recursos/player/nave-left-*.png"),
-            'right': glob.glob("recursos/player/nave-right-*.png"),
-        }
-        self.direcao = "up"
-
-        self.ani["up"].sort()
-        self.ani["left"].sort()
-        self.ani["right"].sort()
-        self.speed = 1
-        self.ani_pos = 0
-        self.ani_max = len(self.ani[self.direcao])-1
-        self.ani_speed_init = 50
-        self.ani_speed = self.ani_speed_init
-        self.img = pygame.image.load(self.ani[self.direcao][self.ani_pos])
-        self.x = (WINDOW_HEIGHT/2)-self.img.get_height()/2
-        self.y = WINDOW_WIDTH-self.img.get_width()
-        self.update()
-
-
-
-    # Método que atualiza a nave frame a frame
-    def update(self):
-        self.ani_speed -= 1
-        if self.ani_speed == 0:
-            self.img = pygame.image.load(self.ani[self.direcao][self.ani_pos])
-            self.ani_speed = self.ani_speed_init
-            if self.ani_pos == self.ani_max:
-                self.ani_pos = 0
-            else:
-                self.ani_pos += 1
-
-        if self.movimento["up"]:
-            if self.y > 0:
-                self.y -= self.speed
-
-        if self.movimento["down"]:
-            if self.y < WINDOW_WIDTH-self.img.get_width():
-                self.y += self.speed
-
-        if self.movimento["left"]:
-            self.direcao = "left"
-            if self.x > 0:
-                self.x -= self.speed
-
-        if self.movimento["right"]:
-            self.direcao = "right"
-            if self.x < WINDOW_HEIGHT - self.img.get_height():
-                self.x += self.speed
-
-        screen.blit(self.img, (self.x, self.y))
-
-    def dano(self):
-        pass
-
-    def get_rect(self):
-        pass
-
-player = Player()
-
-class Interface:
-    def __init__(self):
-        self.font = pygame.font.Font("recursos/font.ttf", 35)
+        self.WINDOW = (800, 600)
+        self.screen = pygame.display.set_mode(self.WINDOW, FULLSCREEN)
+        pygame.init()
+        pygame.mouse.set_visible(False)
+        self.sound = pygame.mixer.Sound('music/musica.wav')
+        self.sound.play()
+        self.font = pygame.font.Font("recursos/font.ttf", 25)
 
     def update(self):
-        #texto = "Aperte ESC para sair!"
-        #mesg = self.font.render(texto, 1, (255, 0, 0))
-        #screen.blit(mesg, (WINDOW_WIDTH/2.5, WINDOW_HEIGHT/2.5))
-        pass
+        vida = "Vida: X X X"
+        mesg = self.font.render(vida, 1, (255,255,255))
+        self.screen.blit(mesg, (10, 10))
+        especial = "X X X : Especial"
+        mesg = self.font.render(especial, 1, (255, 255, 255))
+        self.screen.blit(mesg, (550, 10))
 
-interface = Interface()
+game = Game()
+player = Player(game)
+
 
 while True:
-    screen.fill((0,0,0))
+    game.screen.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
@@ -121,6 +56,7 @@ while True:
             if event.key == K_RIGHT:
                 player.movimento['right'] = False
                 player.direcao = "up"
-    interface.update()
+
     player.update()
+    game.update()
     pygame.display.update()
---
+++ player.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import pygame, glob
+from pygame import *
+
+class Player:
+    def __init__(self, game):
+        self.screen = game.screen
+        self.WINDOW = game.WINDOW
+
+        self.movimento = {
+            'up': False,
+            'down': False,
+            'left': False,
+            'right': False,
+        }
+
+        self.ani = {
+            'up': glob.glob("recursos/player/nave-up-*.png"),
+            'left': glob.glob("recursos/player/nave-left-*.png"),
+            'right': glob.glob("recursos/player/nave-right-*.png"),
+        }
+        self.direcao = "up"
+
+        self.ani["up"].sort()
+        self.ani["left"].sort()
+        self.ani["right"].sort()
+        self.speed = 1
+        self.ani_pos = 0
+        self.ani_max = len(self.ani[self.direcao])-1
+        self.ani_speed_init = 50
+        self.ani_speed = self.ani_speed_init
+        self.img = pygame.image.load(self.ani[self.direcao][self.ani_pos])
+        self.x = (self.WINDOW[0]/2)-self.img.get_height()/2
+        self.y = self.WINDOW[1]-self.img.get_width()
+        self.update()
+
+
+
+    # Método que atualiza a nave frame a frame
+    def update(self):
+        self.ani_speed -= 1
+        if self.ani_speed == 0:
+            self.img = pygame.image.load(self.ani[self.direcao][self.ani_pos])
+            self.ani_speed = self.ani_speed_init
+            if self.ani_pos == self.ani_max:
+                self.ani_pos = 0
+            else:
+                self.ani_pos += 1
+
+        if self.movimento["up"]:
+            if self.y > 0:
+                self.y -= self.speed
+
+        if self.movimento["down"]:
+            if self.y < self.WINDOW[1]-self.img.get_width():
+                self.y += self.speed
+
+        if self.movimento["left"]:
+            self.direcao = "left"
+            if self.x > 0:
+                self.x -= self.speed
+
+        if self.movimento["right"]:
+            self.direcao = "right"
+            if self.x < self.WINDOW[0] - self.img.get_height():
+                self.x += self.speed
+
+        self.screen.blit(self.img, (self.x, self.y))
+
+    def dano(self):
+        pass
+
+    def get_rect(self):
+        pass
